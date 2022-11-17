0a3884bac0e37c059fb22a9e76ac37d66e9c3518
Romain Marziali <romainmarziali@gmail.com>
2022-11-17 22:10:32 +0100

    setup checklist for game filter

--- src/app/story-params/story-params.component.ts
+++ src/app/story-params/story-params.component.ts
@@ -1,30 +1,25 @@
 import { Component } from '@angular/core';
 import { StoryComponent } from '../story/story.component';
 
-import {
-  FormBuilder,
-  FormGroup,
-  FormArray,
-  FormControl,
-  ValidatorFn
-} from '@angular/forms';
-
+interface GameFilter {
+  name: string;
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-story-params',
   templateUrl: './story-params.component.html',
   styleUrls: ['./story-params.component.scss']
 })
+
 export class StoryParamsComponent {
   games: string[] = [];
-
-  // reprendre avec ça puis tenter de basculer sur un angular material (sinon, virer le angular material pour les checkbox) https://www.itsolutionstuff.com/post/angular-dynamic-checkbox-list-exampleexample.html
-  selectedGames: { [key: string]: boolean; } = {}
+  gameFilters: GameFilter[] = [];
 
   constructor(private StoryComponent: StoryComponent) {
     this.games = this.StoryComponent.games;
-    for (const game of this.games) this.selectedGames[game] = true;
-
+    for (const game of this.games) this.gameFilters.push({ name: game, selected: true });
   }
+
 
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,11 @@
-import {Component} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+interface Skill {
+  name: string;
+  selected: boolean;
+  disabled: boolean;
+  subskills?: Skill[];
+}
 
 /** @title Checkboxes with reactive forms */
 @Component({
@@ -8,11 +14,41 @@ import {FormBuilder} from '@angular/forms';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent {
-  toppings = this._formBuilder.group({
-    pepperoni: false,
-    extracheese: false,
-    mushroom: false,
-  });
 
-  constructor(private _formBuilder: FormBuilder) {}
+  title = 'angular-material-checkbox-list';
+
+  allChecked: boolean = false;
+
+  skill: Skill = {
+    name: 'All Skills',
+    selected: false,
+    disabled: false,
+    subskills: [
+      { name: 'HTML', selected: false, disabled: false },
+      { name: 'CSS', selected: true, disabled: false },
+      { name: 'JavaScript', selected: false, disabled: false }
+    ]
+  };
+
+  // Return true if selected items are less then total(allChecked) but more then 0
+  isFewSelected(): boolean {
+    return (this.skill.subskills ? this.skill.subskills.filter(t => t.selected).length > 0 && !this.allChecked : false);
+  }
+
+  // Update Master and list checkboxes
+  setAll(selected: boolean) {
+    this.allChecked = selected;
+    if (this.skill.subskills == null) {
+      return;
+    }
+    this.skill.subskills.forEach(t => t.selected = selected);
+  }
+
+  // Check master checkbox if all items selected
+  updateAllComplete() {
+    console.log(this.skill.subskills);
+    this.allChecked = this.skill.subskills != null && this.skill.subskills.every(t => t.selected);
+  }
 }
+
+
