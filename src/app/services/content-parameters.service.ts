cc217371bcbd0f155e79875e63084c205aa5a785
Romain Marziali <romainmarziali@gmail.com>
2023-08-12 17:15:37 +0200

    Adding local storage for parameters change

--- src/app/services/content-parameters.service.ts
+++ src/app/services/content-parameters.service.ts
@@ -2,16 +2,38 @@ import { Injectable } from '@angular/core';
 import { FiltersService } from 'src/app/services/filters.service';
 import { Filter } from 'src/app/models/filter.model';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class ContentParametersService {
 
   filters: Filter[] = [];
+  settings: undefined;
+  visibility: undefined;
 
   constructor(public filtersService: FiltersService) {
     this.filters = filtersService.getFilters();
+    this.loadParameters();
   }
+
+  saveParameters() {
+    localStorage.setItem('filters', JSON.stringify(this.filters));
+    localStorage.setItem('settings', JSON.stringify(this.settings));
+    localStorage.setItem('visibility', JSON.stringify(this.visibility));
+  }
+
+  loadParameters() {
+    const localFilters = localStorage.getItem('filters');
+    if (localFilters) {
+      const localFiltersParsed = JSON.parse(localFilters);
+      for (const filter of localFiltersParsed) {
+        const filterIndex = this.filters.findIndex(f => f.id === filter.id);
+        if (filterIndex !== -1) {
+          this.filters[filterIndex].selected = filter.selected;
+        }
+      }
+    }
+  }
+
 
 }
