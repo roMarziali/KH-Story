1b6b8f8abb6e7b0461131e2f4371008fada58796
Romain Marziali <romainmarziali@gmail.com>
2024-01-06 11:35:03 +0100

    programation of custom parameters (except night mode)

--- src/app/components/main-page/story/story.component.ts
+++ src/app/components/main-page/story/story.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Segment } from 'src/app/models/segment';
 import { ApiService } from 'src/app/services/api.service';
 import { AnnotationsService } from 'src/app/services/annotations.service';
+import { ContentParametersService } from 'src/app/services/content-parameters.service';
 
 @Component({
   selector: 'app-story',
@@ -12,7 +13,7 @@ export class StoryComponent {
 
   segments: Segment[] = [];
 
-  constructor(private api: ApiService, private annotations: AnnotationsService) { }
+  constructor(private api: ApiService, private annotations: AnnotationsService, private contentParameters: ContentParametersService) { }
 
   ngOnInit() {
     this.annotations.getAnnotations(); // A appeler avant les segments car les segments vont utiliser les annotations
@@ -29,5 +30,18 @@ export class StoryComponent {
       return a.order - b.order;
     });
   }
+
+  getCSS(property: string): string | number | boolean {
+    return this.contentParameters.visibility.find(v => v.id === property)?.value || '';
+  }
+
+  getCSSWidth(): number {
+    const width = this.contentParameters.visibility.find(v => v.id === "width")?.value
+    if (!width || typeof (width) !== "number") return 95;
+    else {
+      return width - 5;
+    }
+  }
+
 
 }
--- src/app/services/visibility.service.ts
+++ src/app/services/visibility.service.ts
@@ -18,7 +18,7 @@ export class VisibilityService {
       label: 'Police',
       type: 'options',
       options: {
-        "Verdana": "Verdaba",
+        "Verdana": "Verdana",
         "Arial": "Arial",
         "Times New Roman": "Times New Roman",
         "Helvetica": "Helvetica",
@@ -33,7 +33,6 @@ export class VisibilityService {
       label: 'Taille de la police',
       type: 'options',
       options: {
-        "10px": "10px",
         "12px": "12px",
         "14px": "14px",
         "16px": "16px",
@@ -44,38 +43,11 @@ export class VisibilityService {
     },
 
     {
-      id: 'columns',
-      type: 'options',
-      label: 'Nombre de colonnes',
-      options: {
-        "1": "1",
-        "2": "2",
-        "3": "3",
-        "4": "4",
-        "5": "5"
-      },
-      value: "1"
-    },
-
-    {
       id: 'width',
       label: 'Largeur de la page',
       type: 'range',
       value: 100
     },
-
-    {
-      id: 'flex-align',
-      label: 'Emplacement du texte',
-      type: 'options',
-      options: {
-        "flex-start": "À gauche",
-        "center": "Au centre",
-        "flex-end": "À droite"
-      },
-      value: "flex-start"
-    },
-
   ];
 
   getVisibility(): Visibility[] {
