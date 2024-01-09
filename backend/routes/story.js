3701aaf22311759963aa849781c487e05eb7af03
Romain Marziali <romainmarziali@gmail.com>
2024-01-09 21:10:36 +0100

    Adding log for segments loading

--- backend/routes/story.js
+++ backend/routes/story.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const SegmentManager = require("../models/segment-manager");
+const { logSegmentsConsultation } = require("../models/logger");
 
 router.get("/segments", async (req, res, next) => {
   const segments = await SegmentManager.getSegments();
@@ -8,6 +9,8 @@ router.get("/segments", async (req, res, next) => {
 });
 
 router.get("/annotations", async (req, res, next) => {
+  const userWebAgent = req.headers['user-agent'];
+  logSegmentsConsultation(userWebAgent);
   const annotations = await SegmentManager.getAnnotations();
   res.send(annotations);
 });
