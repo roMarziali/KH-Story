6dda9a971c70c391267cf5bd576cc0ef25ca488b
Romain Marziali <romainmarziali@gmail.com>
2024-01-27 04:28:54 +0100

    Add a comment for the last commit

--- src/app/services/story.service.ts
+++ src/app/services/story.service.ts
@@ -119,6 +119,7 @@ export class StoryService {
         paragraphs: this.getParagraphsFromRawSection(rawSection)
       };
       if (section.paragraphs.length > 0 || this.authService.isAuthenticated) sections.push(section);
+      // on autorise l'affichage des sections vides si l'utilisateur est connecté sinon il ne pourrait pas créer un paragraphe dans une section vide
     });
     return sections;
   }
