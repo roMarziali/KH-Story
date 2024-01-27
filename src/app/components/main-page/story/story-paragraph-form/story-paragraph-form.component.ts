995bc0d0ec6b2e7559ed9c2075245a22b8a19932
Romain Marziali <romainmarziali@gmail.com>
2024-01-27 21:08:19 +0100

    paragraph form : adding possibility to generate multiple texts and text field

--- src/app/components/main-page/story/story-paragraph-form/story-paragraph-form.component.ts
+++ src/app/components/main-page/story/story-paragraph-form/story-paragraph-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { TextFormMetadata } from 'src/app/models/text-form-identifier';
 import { ApiService } from 'src/app/services/api.service';
-import { FormGroup, FormControl, Validators, ValidationErrors } from "@angular/forms";
+import { FormGroup, FormControl, Validators, ValidationErrors, FormArray } from "@angular/forms";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 
 @Component({
@@ -15,16 +15,41 @@ export class StoryParagraphFormComponent {
   action!: "adding" | "editing";
 
   paragraphForm = new FormGroup({
-    paragraph: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    texts: new FormArray([]),
   });
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { textFormMetadata: TextFormMetadata, action: "adding" | "editing" }, private api: ApiService, public dialogRef: MatDialogRef<any>) {
     this.textFormMetadata = data.textFormMetadata;
     this.action = data.action;
+    if (this.action == "adding") {
+      this.addTextToForm();
+    }
+  }
+
+  addTextToForm() {
+    const newTextSubForm = new FormGroup({
+      text: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      relatedTo: new FormControl([], [Validators.required, Validators.minLength(1)]),
+      image: new FormGroup({
+        game: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        alt: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      }),
+    });
+    (this.paragraphForm.get('texts') as FormArray).push(newTextSubForm);
   }
 
   onSubmitParagraph() {
+    console.log(this.paragraphForm.value);
     if (this.paragraphForm.invalid) return;
     console.log("submission");
+  }
+
+  deleteTextFromForm(index: number) {
+    (this.paragraphForm.get('texts') as FormArray).removeAt(index);
+  }
+
+  get texts() {
+    return this.paragraphForm.controls.texts;
   }
 }
