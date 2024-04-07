c3ee9d87c4bf8e1a62d4b04e02b43b576ee89eea
Romain Marziali <romainmarziali@gmail.com>
2024-04-07 11:26:10 +0200

    Adding component for bibliography

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { BibliographyComponent } from './components/bibliography/bibliography.component';
 
 const routes: Routes = [
   { path: "chapitre/:order", component: MainPageComponent },
   { path: '', redirectTo: '/chapitre/1', pathMatch: 'full'},
+  { path: "bibliographie", component: BibliographyComponent },
+  { path: '**', redirectTo: '/chapitre/1' }
+
 ];
 
 @NgModule({
---
+++ src/app/components/bibliography/bibliography.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bibliography',
+  standalone: true,
+  imports: [],
+  templateUrl: './bibliography.component.html',
+  styleUrl: './bibliography.component.scss'
+})
+export class BibliographyComponent {
+
+}
